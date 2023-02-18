db565f3cb6f944cd97dc72f81ccabb582d7e68c7
Aman Singh <107499592+AmAnSingH8889979919@users.noreply.github.com>
2023-02-18 23:49:47 +0530

    Update index.js

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ const server = http.createServer(app);
 
 const io = new Server( server,{
     cors: {
-    origin: "http://localhost:3000",
+    origin: "https://startling-lily-67aaf1.netlify.app/",
     methods: ["GET", "POST"]
   }
 });
